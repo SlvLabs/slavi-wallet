c5d1cdf83162b3f63f459444b7f44df09b0b0cc0
aldragon <dimonk280894@gmail.com>
2022-02-21 13:03:08 +0500

    Styles fixes

--- src/containers/defi/defi-screen.tsx
+++ src/containers/defi/defi-screen.tsx
@@ -107,63 +107,65 @@ const DefiScreen = () => {
     <SafeAreaView style={styles.container}>
       <ImageBackground source={loadingBackground} style={styles.background}>
         <RadialGradient style={styles.gradient} {...theme.gradients.radialLoadingGradient}>
-          <Image
-            source={screenStub}
-            width={Layout.isSmallDevice ? 205 : 310}
-            height={Layout.isSmallDevice ? 165 :245}
-            style={{
-              width: Layout.isSmallDevice ? 205 : 310,
-              height: Layout.isSmallDevice ? 165 :245}}
-          />
-          <Text style={styles.header}>{t('Slavi DeFi is coming...')}</Text>
-          <View style={styles.descriptionContainer}>
-            <Text style={styles.description}>{t('Comprehensive with versatile functionality,')}</Text>
-            <Text style={styles.description}>{t('the first version of Slavi DeFi is coming.')}</Text>
-            <Text style={styles.description}>{t('Stay tunes in Newsletter.')}</Text>
-          </View>
-          {(!!data && !isLoading) && (
-            <View style={{width: '100%'}}>
-              <KeyboardAwareScrollView style={styles.inputContainer}>
-                <TextInput
-                  placeholder={t('Your Email')}
-                  style={{...styles.input, borderColor: border}}
-                  placeholderTextColor={theme.colors.textLightGray}
-                  editable={!subscribed}
-                  selectTextOnFocus={!subscribed}
-                  textContentType={'emailAddress'}
-                  value={email}
-                  onChangeText={onEmailChange}
-                />
-                {subscribed && (
-                  <CustomIcon
-                    name={'check'}
-                    color={theme.colors.green}
-                    size={14}
-                    style={styles.icon}
+          <KeyboardAwareScrollView>
+            <Image
+              source={screenStub}
+              width={Layout.isSmallDevice ? 205 : 310}
+              height={Layout.isSmallDevice ? 165 :245}
+              style={{
+                width: Layout.isSmallDevice ? 205 : 310,
+                height: Layout.isSmallDevice ? 165 :245}}
+            />
+            <Text style={styles.header}>{t('Slavi DeFi is coming...')}</Text>
+            <View style={styles.descriptionContainer}>
+              <Text style={styles.description}>{t('Comprehensive with versatile functionality,')}</Text>
+              <Text style={styles.description}>{t('the first version of Slavi DeFi is coming.')}</Text>
+              <Text style={styles.description}>{t('Stay tunes in Newsletter.')}</Text>
+            </View>
+            {(!!data && !isLoading) && (
+              <View style={{width: '100%'}}>
+                <View style={styles.inputContainer}>
+                  <TextInput
+                    placeholder={t('Your Email')}
+                    style={{...styles.input, borderColor: border}}
+                    placeholderTextColor={theme.colors.textLightGray}
+                    editable={!subscribed}
+                    selectTextOnFocus={!subscribed}
+                    textContentType={'emailAddress'}
+                    value={email}
+                    onChangeText={onEmailChange}
+                  />
+                  {subscribed && (
+                    <CustomIcon
+                      name={'check'}
+                      color={theme.colors.green}
+                      size={14}
+                      style={styles.icon}
+                    />
+                  )}
+                </View>
+                {!!error && <Text style={styles.error}>{t(error as TranslationsKey)}</Text>}
+                {(data?.subscribed?.find((element) => element.type == SUBSCRIBE_KEY) || subscribed) ? (
+                  <Text style={styles.subscribedText}>{t('You\'ve successfully subscribed')}</Text>
+                ) : (
+                  <SolidButton
+                    icon={
+                      <CustomIcon
+                        name={'arrow-right'}
+                        color={theme.colors.white}
+                        size={14}
+                        style={styles.buttonIcon}
+                      />
+                    }
+                    iconRight={true}
+                    title={t('Subscribe')}
+                    onPress={subscribe}
+                    containerStyle={styles.buttonContainer}
+                    disabled={isLoading || postLoading || !!error}
                   />
                 )}
-              </KeyboardAwareScrollView>
-              {!!error && <Text style={styles.error}>{t(error as TranslationsKey)}</Text>}
-              {(data?.subscribed?.find((element) => element.type == SUBSCRIBE_KEY) || subscribed) ? (
-                <Text style={styles.subscribedText}>{t('You\'ve successfully subscribed')}</Text>
-              ) : (
-                <SolidButton
-                  icon={
-                    <CustomIcon
-                      name={'arrow-right'}
-                      color={theme.colors.white}
-                      size={14}
-                      style={styles.buttonIcon}
-                    />
-                  }
-                  iconRight={true}
-                  title={t('Subscribe')}
-                  onPress={subscribe}
-                  containerStyle={styles.buttonContainer}
-                  disabled={isLoading || postLoading || !!error}
-                />
-              )}
-          </View>)}
+            </View>)}
+          </KeyboardAwareScrollView>
         </RadialGradient>
       </ImageBackground>
     </SafeAreaView>

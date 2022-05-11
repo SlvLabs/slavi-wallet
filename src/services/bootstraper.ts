2210b465671089f38cf8eafc51a7e9a13053254b
Кондрашин Дмитрий Вячеславович <dmitrijj.kondrashin@vseinstrumenti.ru>
2022-05-11 12:52:18 +0500

    version 1.5.1

--- src/services/bootstraper.ts
+++ src/services/bootstraper.ts
@@ -1,14 +1,11 @@
 import Config from 'react-native-config';
 import CoreBootstrap from '@slavi/wallet-core/src/utils/bootstrap';
-// @ts-ignore
-import i18nextReactNative from 'i18next-react-native-language-detector';
 import {Store} from 'redux';
 import {networks} from '@slavi/crypto-core';
 import {DataStoreProviderInterface, ServiceLocatorCoreInterface} from '@slavi/wallet-core/src/types';
 import {CoinsServiceConf} from '@slavi/wallet-core/src/services/coins-service';
 import SimpleToast from 'react-native-simple-toast';
 import PerformanceMonitorInterface from "@slavi/wallet-core/src/utils/performance-monitor-interface";
-import { ContractAbiProviderConf } from '@slavi/wallet-core/src/services/contract-abi-provider';
 import translations from '../assets/translations/fallback';
 
 
@@ -20,10 +17,6 @@ const wsConfig = {
     timeout: +Config.WS_PING_TIMEOUT || 31000,
     route: Config.WS_PING_ROUTE,
   },
-};
-
-const abiProviderConfig: ContractAbiProviderConf = {
-  diskCacheEnabled: !!Config.DISK_CACHE || true,
 };
 
 const coinsServiceConfig: CoinsServiceConf = {
@@ -47,12 +40,11 @@ const bootstrap = async (
 ): Promise<void> => {
   const coreBootstrap = new CoreBootstrap({
     wsConfig: wsConfig,
-    systemLanguage: i18nextReactNative.detect(),
+    systemLanguage: 'en',
     store: store,
     authCoinDerivedPath: Config.AUTH_DERIVED_PATH,
     authCoinNetwork: networks.AUTH_SLV,
     dataStorageProvider: dataStorageProvider,
-    abiProviderConfig: abiProviderConfig,
     coinsServiceConfig: coinsServiceConfig,
     performanceMonitor: performanceMonitor,
     onError: err =>

72b2dac6a157a7fa1524d95ce5a618bde0889056
artem <babkin.artem@gmail.com>
2022-09-05 10:20:28 +0500

    hotfix

--- src/containers/nft/nft-send-screen.tsx
+++ src/containers/nft/nft-send-screen.tsx
@@ -265,8 +265,6 @@ const styles = StyleSheet.create({
     color: theme.colors.white,
     marginTop: 16,
     marginBottom: 20,
-    borderColor: '#fff',
-    borderWidth: 1,
     textAlign: "center",
   },
   button: {
